130e1bb27a14aa63ea11b46515a2517079cc31e6
Luis <edububa@gmail.com>
2016-08-27 13:17:38 +0200

    machine.hs refactored

--- Painting Machine/src/machine.hs
+++ Painting Machine/src/machine.hs
@@ -1,12 +1,25 @@
+import Data.List.Split
+
+indexList :: [Char] -> [(Int, [Char])]
+indexList xs = zip [0..] $ listOfTwo xs
+  where
+    listOfTwo  = \x -> if length x == 1 then [x] else takeTwo x : listOfTwo (tail x)
+    takeTwo    = \(x:xs) -> x : head xs : []
+
+lineStarts :: [Char] -> [Int]
+lineStarts xs = if head xs == '#' then 0 : f xs else f xs
+  where
+    start = \x -> x /= [] && length x > 1 && head x == '.' && head (tail x) == '#'
+    f     = \x -> foldr (\x acc -> if start (snd x) then (fst x + 1) : acc else acc) [] $ indexList x
+
+lineEnds :: [Char] -> [Int]
+lineEnds xs = if last xs == '#' then g xs ++ [length xs - 1] else g xs
+  where
+    end = \x -> x /= [] && length x > 1 && head x == '#' && head (tail x) == '.'
+    g   = \x -> foldr (\x acc -> if end (snd x) then (fst x) : acc else acc) [] $ indexList x
+    
 lineAnalyzer :: [Char] -> [(Int,Int)]
 lineAnalyzer xs = zip (lineStarts xs) (lineEnds xs)
-  where
-    indexList  = \x -> zip [0..] $ listOfTwo x
-    takeTwo    = \x -> head x : head (tail x) : []
-    lineStarts = \x -> if head x == '#' then 0 : f x else f x
-    lineEnds   = \x -> if last x == '#' then g x ++ [length xs - 1] else g x
-    listOfTwo  = \x -> if length x == 1 then [x] else takeTwo x : listOfTwo (tail x)
-    g          = \x -> foldr (\x acc -> if end (snd x) then (fst x) : acc else acc) [] $ indexList x
-    start      = \x -> if x == [] || length x <= 1 then False else head x == '.' && head (tail x) == '#'
-    end        = \x -> if x == [] || length x <= 1 then False else head x == '#' && head (tail x) == '.'
-    f          = \x -> foldr (\x acc -> if start (snd x) then (fst x + 1) : acc else acc) [] $ indexList x
+
+inputAnalyzer ::[Char] -> [(Int, [(Int, Int)])]
+inputAnalyzer xs = zip [0..] $ map lineAnalyzer (splitOn "\n" xs)
--- challenges/reversenumber.hs
+++ challenges/reversenumber.hs
@@ -1,23 +1,13 @@
 -- challenge 70: reverse a number
 reversenumber :: Int -> Int
-reversenumber x = reversenumberaux x (ncifras x)
-
-ncifras :: Int -> Int
-ncifras x =
-  if div x 10 == mod x 10 then 0
-  else 1 + ncifras(div x 10)
-  
-reversenumberaux :: Int -> Int -> Int
-reversenumberaux x n =
-  if (n - 1) == 0 then x
-  else (mod x 10) * 10 ^ (n - 1) + reversenumberaux (div x 10) (n - 1)
+reversenumber x = f x (ncifras x)
+  where
+    ncifras = \x -> if div x 10 == mod x 10 then 0 else 1 + ncifras(div x 10)
+    f = \x n -> if (n - 1) == 0 then x else (mod x 10) * 10 ^ (n - 1) + f (div x 10) (n - 1)
 
 -- other implementation
 reversenumberf :: Int -> Int
 reversenumberf x =
-  foldr (+) (0) (zipWith (*) (reverse (map (10^) [0..((length (splitrevnumber x)) - 1)])) (splitrevnumber x))
-
-splitrevnumber :: Int -> [Int]
-splitrevnumber x =
-  if (mod x 10) == x && (div x 10) == 0 then [x]
-  else (mod x 10) : splitrevnumber (div x 10)
+  foldr (+) (0) (zipWith (*) (reverse (map (10^) [0..((length (splitrevn x)) - 1)])) (splitrevn x))
+  where
+    splitrevn = \x -> if (mod x 10) == x && (div x 10) == 0 then [x] else (mod x 10) : splitrevn (div x 10)
